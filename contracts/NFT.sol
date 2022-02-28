66488e154bbfd809b49ac0ab6a1868c4e5a0ffdc
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2022-02-28 11:10:42 +0200

    Initial commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC721_prod is ERC721('NFT Token Name', "NFT"), Ownable { //TODO: change name and ticker
+  string private baseURI;
+  uint256 private totalSupply;
+
+  constructor (string memory baseURI_) {
+    baseURI = baseURI_;
+  }
+
+  function mint(address _address) external payable onlyOwner {
+    require(msg.value > 0.01 ether, "Invalid NFT buy price amount");
+
+    _mint(_address, totalSupply++);
+  }
+
+
+  function setBaseURI(string calldata uri) external onlyOwner {
+      baseURI = uri;
+  }
+
+  function _baseURI() internal override  view returns (string memory) {
+      return baseURI;
+  }
+
+}
